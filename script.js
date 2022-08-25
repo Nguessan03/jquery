3a884a239954e86c7ca69e5f140048e60ab2dcd9
Nguessan03 <108245015+Nguessan03@users.noreply.github.com>
2022-08-25 10:15:00 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+$.when($.ready).then(function(){
+
+    jQuery('#show').click(function(){
+
+$(':header, li, p').show(10000);
+
+    });
+
+    $("#hide").click(function(){
+
+        $(':header,li, p').hide(10000);
+
+    });
+
+    $('body').css('color','#F0F8FF')
+    jQuery(":header:not(h3)").css('color','#000080');
+    jQuery("h3").css('color','#4682E1');
+    jQuery("ul li").css('color','#1E90FF');
+    $('p').css('color','#191970');
+    $(img)
+
+    });
+
+
